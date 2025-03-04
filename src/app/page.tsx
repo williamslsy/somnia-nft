080dbdab5cb9b6bc580084c08a391a3e31d8f4e5
dev_waliba <donwaleyb@gmail.com>
2025-03-04 03:17:55 +0100

    Add Header component to Home page for improved layout and navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import { Header } from '@/components/header';
+
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-6xl font-bold bg-black">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
-    </div>
+    <main className="flex flex-col min-h-screen">
+      <Header />
+      <div className="flex flex-col items-center justify-center flex-1 py-2">
+        <h1 className="text-6xl font-bold">
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+      </div>
+    </main>
   );
 }
